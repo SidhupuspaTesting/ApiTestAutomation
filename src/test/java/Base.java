acaf2438bd8dbf64944c3cbfe5ad603c856b2ce4
SidhupuspaTesting <sidhupuspa1@gmail.com>
2025-11-08 00:49:54 +0530

    validating the response where scope = app and header name

--- src/test/java/Base.java
+++ src/test/java/Base.java
@@ -1,9 +1,12 @@
 import io.restassured.RestAssured;
+import static org.hamcrest.Matchers.*;
 import static io.restassured.RestAssured.*;
+
 public class Base {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Now we will validate if in response we are getting APP or not . We will validate the output 
 RestAssured.baseURI="https://rahulshettyacademy.com";
 given().log().all().queryParam("key","qaclick123").header("Content-Type", "application/json")
 .body("{\r\n"
@@ -23,8 +26,14 @@ given().log().all().queryParam("key","qaclick123").header("Content-Type", "appli
 		+ "  \"language\": \"French-IN\"\r\n"
 		+ "}\r\n"
 		+ " \r\n"
-		+ "").when().log().all().post("maps/api/place/add/json").then().log().all().assertThat().statusCode(200);
+		+ "").when().log().all().post("maps/api/place/add/json").then().log().all().assertThat()
+.statusCode(200).body("scope",equalTo( "APP")).header("server", "Apache/2.4.52 (Ubuntu)");
+//we are validating the response where scope = app coming or not ,
+//and validating one header response let ex: Servername in header server is apache or not
+//and equals to is a static method so need to import manually
+//isequals to can imported from org.hamcrest.Matchers.*;	
 
-	}
+
+	} 	
 
 }
