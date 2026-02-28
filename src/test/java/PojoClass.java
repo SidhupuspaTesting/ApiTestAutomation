1cbcd90f3c5602fc54630758a92b040a8b24109f
SidhupuspaTesting <sidhupuspa1@gmail.com>
2026-02-28 08:41:44 +0530

    OAuth test

---
+++ src/test/java/PojoClass.java
@@ -0,0 +1,26 @@
+
+
+public class PojoClass {
+	
+private String message;
+private String greet;
+
+public String getMessage()
+{
+	return message;
+	
+}
+public void setMessage(String messages)
+{
+	this.message=messages;
+}
+public String getGreet()
+{
+	return greet;
+	
+}
+public void getGreet(String greets)
+{
+	this.greet=greets;
+}
+}
